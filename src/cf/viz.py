b42847031f5807ee7626fb58f349093c631d7e60
mamaofthree579-ship-it <mamaofthree579@gmail.com>
2025-10-23 12:39:47 -0400

    Update viz.py

--- src/cf/viz.py
+++ src/cf/viz.py
@@ -1,3 +1,8 @@
-import matplotlib.pyplot as plt
-def plot_timeseries(times, values, outpath):
-    plt.figure(); plt.plot(times, values); plt.savefig(outpath); plt.close()
+import numpy as np
+import plotly.graph_objects as go
+
+def visualize_waveform(x, y, title="Harmonic Field"):
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=x, y=y, mode="lines"))
+    fig.update_layout(title=title, xaxis_title="x", yaxis_title="Amplitude")
+    fig.show()
